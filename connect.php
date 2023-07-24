d4107ef51a5d621afaec43fba6308b561c9ab533
Teena-kachi <teenakachi2002@gmail.com>
2023-07-24 18:00:31 +0530

    first commit

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $db_host = "localhost";
 $db_user = "root";
-$db_password = "Admin@1234";
+$db_password = "";
 $db_name = "animal_db";
 
 $conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
